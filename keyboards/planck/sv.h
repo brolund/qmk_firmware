3e8f3be1ca2dcdd11dd464979f339583ebd97ca7
Daniel Brolund <daniel.brolund@agical.com>
2017-04-02 18:48:10 +0200

    Added my keymap based on callum

---
+++ keyboards/planck/sv.h
@@ -0,0 +1,13 @@
+#ifndef SV_H
+#define SV_H
+
+#include "planck.h"
+#include "keymap_extras/keymap_nordic.h"
+
+#define SV_OE KC_SCLN
+#define SV_AE KC_QUOT
+#define SV_AA KC_LBRC
+
+#define SV_PIPE ALGR(NO_LESS)
+#define SV_BSLS ALGR(NO_PLUS)
+#endif
