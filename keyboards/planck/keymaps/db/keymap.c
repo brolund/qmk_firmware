750823cc60a730ce71bdfe2fe1d853c4ec2ed33e
Daniel Brolund <daniel.brolund@agical.com>
2017-11-08 16:21:41 +0100

    Indenting

--- keyboards/planck/keymaps/db/keymap.c
+++ keyboards/planck/keymaps/db/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = {
-  { SWL,  SV_AE,   KC_Q,   KC_DOT,  KC_COMM,  KC_Y,    KC_F,    KC_G,    KC_L,    KC_H,    KC_M,    KC_BSPC },
+  { SWL,     SV_AE,   KC_Q,   KC_DOT,  KC_COMM,  KC_Y,    KC_F,    KC_G,    KC_L,    KC_H,    KC_M,    KC_BSPC },
   { KC_TAB,  KC_O,    KC_I,    KC_E,    KC_A,    KC_U,    KC_D,    KC_S,    KC_T,    KC_R,    KC_N,    KC_P },
   { NO_MINS, SV_OE,   KC_J,    KC_K,    SV_AA,   KC_Z,    KC_B,    KC_C,    KC_X,    KC_V,    KC_W,    NO_SCLN },
   THUMB_ROW
