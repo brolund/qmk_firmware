6217e797def36b6bdeff92e7764457140ddc6ee7
Daniel Brolund <daniel.brolund@agical.com>
2017-11-11 23:39:14 +0100

    Better swedish parens

--- keyboards/planck/sv.h
+++ keyboards/planck/sv.h
@@ -10,4 +10,15 @@
 
 #define SV_PIPE ALGR(NO_LESS)
 #define SV_BSLS ALGR(NO_PLUS)
+
+#define SV_LPRN LSFT(KC_8)
+#define SV_RPRN LSFT(KC_9)
+#define SV_LBRC ALGR(KC_7)
+#define SV_RBRC ALGR(KC_0)
+#define SV_LSBR ALGR(KC_8)
+#define SV_RSBR ALGR(KC_9)
+#define SV_LABR KC_BSLS
+#define SV_RABR LSFT(KC_BSLS)
+
+
 #endif
