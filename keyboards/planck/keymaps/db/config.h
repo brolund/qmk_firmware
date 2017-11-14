5b1b0436eda8fab9d5da0754a057902f733be869
Daniel Brolund <daniel.brolund@agical.com>
2017-11-14 22:48:21 +0100

    Volume buttons

--- keyboards/planck/keymaps/db/config.h
+++ keyboards/planck/keymaps/db/config.h
@@ -1,5 +1,5 @@
-#define MOUSEKEY_DELAY             20
-#define MOUSEKEY_INTERVAL          20
+#define MOUSEKEY_DELAY             100
+#define MOUSEKEY_INTERVAL          10
 #define MOUSEKEY_MAX_SPEED         5
 #define MOUSEKEY_TIME_TO_MAX       20
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
