3192b9c69254c4e692a6f919523a683b39389b0d
Daniel Brolund <daniel.brolund@agical.com>
2018-06-29 12:23:57 +0200

    Better mouse speed

--- keyboards/planck/keymaps/db/config.h
+++ keyboards/planck/keymaps/db/config.h
@@ -1,7 +1,7 @@
-#define MOUSEKEY_DELAY             150
-#define MOUSEKEY_INTERVAL          50
+#define MOUSEKEY_DELAY             30
+#define MOUSEKEY_INTERVAL          20
 
-#define MOUSEKEY_MAX_SPEED         30
+#define MOUSEKEY_MAX_SPEED         5
 #define MOUSEKEY_TIME_TO_MAX       20
 #define MOUSEKEY_WHEEL_MAX_SPEED   6
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
